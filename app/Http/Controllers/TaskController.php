a78557d942fe6726dc1a0a716695a672735787d5
ESPORTS-128\cre <t-shirai@oca.ac.jp>
2022-11-15 16:18:01 +0900

    11/15 commit kadai 1 complete

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,10 +15,10 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::where('status', false)->get();
-       
-        return view('tasks.index', compact('tasks'));
-      }
+      $tasks = Task::where('status', false)->get();
+     
+      return view('tasks.index', compact('tasks'));
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -39,34 +39,30 @@ class TaskController extends Controller
     public function store(Request $request)
     {
  
-        $rules = [
-          'task_name' => 'required|max:100',
-        ];
-       
-        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-       
-        Validator::make($request->all(), $rules, $messages)->validate();
-       
-       
-        
-        //モデルをインスタンス化
-        $task = new Task;
-       
-        //モデル->カラム名 = 値 で、データを割り当てる
-        $task->name = $request->input('task_name');
-       
-        //データベースに保存
-        $task->save();
-       
-        //リダイレクト
-        return redirect('/tasks');
-      }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+      $rules = [
+        'task_name' => 'required|max:100',
+      ];
+     
+      $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+     
+      Validator::make($request->all(), $rules, $messages)->validate();
+     
+     
+      
+      //モデルをインスタンス化
+      $task = new Task;
+     
+      //モデル->カラム名 = 値 で、データを割り当てる
+      $task->name = $request->input('task_name');
+     
+      //データベースに保存
+      $task->save();
+     
+      //リダイレクト
+      return redirect('/tasks');
+    }
+ 
+
     public function show($id)
     {
         //
@@ -80,7 +76,8 @@ class TaskController extends Controller
      */
     public function edit($id)
     {
-        //
+      $task = Task::find($id);
+      return view('tasks.edit', compact('task'));
     }
 
     /**
@@ -93,42 +90,42 @@ class TaskController extends Controller
     public function update(Request $request, $id)
     {
   
-        //「編集する」ボタンをおしたとき
-        if ($request->status === null) {
-          $rules = [
-            'task_name' => 'required|max:100',
-          ];
-      
-          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-      
-          Validator::make($request->all(), $rules, $messages)->validate();
-      
-      
-          //該当のタスクを検索
-          $task = Task::find($id);
-      
-          //モデル->カラム名 = 値 で、データを割り当てる
-          $task->name = $request->input('task_name');
-      
-          //データベースに保存
-          $task->save();
-        } else {
-          //「完了」ボタンを押したとき
-      
-          //該当のタスクを検索
-          $task = Task::find($id);
-      
-          //モデル->カラム名 = 値 で、データを割り当てる
-          $task->status = true; //true:完了、false:未完了
-      
-          //データベースに保存
-          $task->save();
-        }
-      
-      
-        //リダイレクト
-        return redirect('/tasks');
+      //「編集する」ボタンをおしたとき
+      if ($request->status === null) {
+        $rules = [
+          'task_name' => 'required|max:100',
+        ];
+    
+        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+    
+        Validator::make($request->all(), $rules, $messages)->validate();
+    
+    
+        //該当のタスクを検索
+        $task = Task::find($id);
+    
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $task->name = $request->input('task_name');
+    
+        //データベースに保存
+        $task->save();
+      } else {
+        //「完了」ボタンを押したとき
+    
+        //該当のタスクを検索
+        $task = Task::find($id);
+    
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $task->status = true; //true:完了、false:未完了
+    
+        //データベースに保存
+        $task->save();
       }
+    
+    
+      //リダイレクト
+      return redirect('/tasks');
+    }
      
 
     /**
@@ -139,6 +136,8 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+      Task::find($id)->delete();
+    
+      return redirect('/tasks');
     }
 }
---
+++ storage/framework/views/77199952fe8b05b39b0add155a6527de2e825c4e.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>テスト</title>
+ 
+</head>
+ 
+<body>
+ 
+    <?php if($todo_lists->isNotEmpty()): ?>
+        <div class="container px-5 mx-auto">
+            <ul class="font-medium text-gray-900 bg-white rounded-lg border border-gray-200">
+                <?php $__currentLoopData = $todo_lists; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li class="py-4 px-5 w-full rounded-t-lg border-b last:border-b-0 border-gray-200">
+                        <?php echo e($item->name); ?>
+
+                    </li>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+ 
+</body>
+ 
+</html><?php /**PATH C:\Users\CRE\Desktop\TODO-App-master\resources\views/todo_list/index.blade.php ENDPATH**/ ?>
